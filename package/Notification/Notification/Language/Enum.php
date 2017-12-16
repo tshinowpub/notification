c0349deb942a4cbba354bf98ede80da2bfc9da84
DESKTOP-KTLHJL3\shinohara <wayfarermusic0130@gmail.com>
2017-12-17 04:06:06 +0900

    マスタを管理するための列挙型を作成した

---
+++ package/Notification/Notification/Language/Enum.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Language;
+
+/**
+ * 列挙型
+ *
+ */
+abstract class Enum
+{
+    /**
+     * Enum value
+     *
+     * @var mixed
+     */
+    protected $value;
+
+    public function __construct($value)
+    {
+       if (!is_scalar($value)) {
+           throw new \InvalidArgumentException('This value is not supported. must be use scalar');
+       }
+
+       if(!static::contains($value)) {
+           throw new \DomainException('This value is not allowed.');
+       }
+
+       $this->value = $value;
+    }
+
+    /**
+     * 値を返す
+     *
+     * @var mixed
+     */
+    public function value()
+    {
+        return $this->value;
+    }
+
+    /**
+     * 値を比較する
+     */
+    final public function equals(Enum $enum)
+    {
+        return ($this instanceof $enum || $enum instanceof $this)
+            && $this->value() === $enum->value();
+    }
+
+    /**
+     * 定義されている値をオブジェクトで返す
+     *
+     */
+    public static function values()
+    {
+        $instances = [];
+
+        $class = new \ReflectionClass(static::class);
+
+        foreach ($class->getConstants() as $constant) {
+            $instances[] = new static($constant);
+        }
+
+        return $instances;
+    }
+
+    /**
+     * 指定した値からオブジェクトを取得する
+     *
+     */
+    public static function valueOf($value)
+    {
+        return new static($value);
+    }
+
+    /**
+     * 文字列としてアクセスされたときに値を返す
+     *
+     */
+    public function __toString()
+    {
+        return (string)$this->value;
+    }
+
+    /**
+     * 定義されている値かどうかをチェックする
+     *
+     */
+    private static function contains($value)
+    {
+        $class = new \ReflectionClass(static::class);
+
+        return in_array(
+            $value,
+            $class->getConstants(),
+            true
+        );
+    }
+}
